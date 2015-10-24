13a3c8538befc1ab84cdd2112db2cfdc71e1ba7e
clarencechen <clarencechenct@gmail.com>
2015-10-24 16:13:34 -0700

    Added tempo control and revamped volume control

--- leapinterp.js
+++ leapinterp.js
@@ -1,9 +1,19 @@
-var text = document.getElementById('hand');
-Leap.loop(function (frame) {
+var tempo = 96;
+var volume = 128;
+var volumebuffer;
+var posbuffer;
+var controller = Leap.loop({enableGestures: true}, function (frame) {
 	if(frame.hands.length > 0)
 	{
-
 		var dhand = frame.hands[0];
+		var position = dhand.palmPosition[1];
+		if(controller.frame(1).hands.length == 0)
+		{
+			volumebuffer = volume;
+			posbuffer = position;
+		}
+		volume = 0.1*(position -posbuffer) + volumebuffer;
+		volume = Math.round(volume);
 		if(dhand.grabStrength > 0.8)
 		{
 			pause.innerHTML = 'Paused';
@@ -14,8 +24,24 @@ Leap.loop(function (frame) {
 			player.resume();
 			pause.innerHTML = '';
 		}
-		var position = dhand.palmPosition;
-		hand.innerHTML = 'Volume: ' + position[1];
 	}
-	
+	if(frame.valid && frame.gestures.length > 0)
+	{
+    	frame.gestures.forEach(function(gesture) {
+    		if(gesture.type == "circle")
+			{
+				var clockwise = false;
+				var pointableID = gesture.pointableIds[0];
+				var direction = frame.pointable(pointableID).direction;
+				var dotProduct = Leap.vec3.dot(direction, gesture.normal);
+				if(dotProduct > 0)
+					clockwise = true;
+				var circleProgress = clockwise ? gesture.progress : -gesture.progress;
+				tempo *= Math.pow(Math.pow(2,1/64),circleProgress);
+				tempo = Math.round(tempo);
+			}
+    	});
+	}
+	first.innerHTML = 'Volume: ' + volume + ' ';
+	second.innerHTML = 'Tempo: ' + tempo + ' ';
 });
